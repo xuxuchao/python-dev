4c3718cde5b048c4160a371eff6640b5a1f751d9
xuxuchao <45984350+xuxuchao@users.noreply.github.com>
2019-11-25 20:43:15 +0800

    3
    3

--- Chapter-01-code/phonebook_func_json.py
+++ Chapter-01-code/phonebook_func_json.py
@@ -1,31 +1,95 @@
-<html>
-<head>
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-</head>
-<body>
-
-
+作业
+有如下html
 <div id="test-div">
   <p id="test-js">javascript</p>
   <p>Java</p>
 </div>
+获取<p>javascript</p>
+修改文本为JavaScript
+修改CSS为: color: #ff0000, font-weight: bold
+注： 使用javascript 和jquery实现
 
-<script>
-var js = $("#test-js");
-js.text("JavaScript")
-js.css("color","#ff0000")
-</script>
-
-</body>
-</html>
-
-<div id="test-div">
-  <p id="test-js">javascript</p>
-  <p>Java</p>
+有如下html
+<ul id="test-list">
+    <li>JavaScript</li>
+    <li>Swift</li>
+    <li>HTML</li>
+    <li>ANSI C</li>
+    <li>CSS</li>
+    <li>DirectX</li>
+</ul>
+把与Web开发技术不相关的节点删掉 注： 使用javascript和jquery实现
+有如下html
+<div class="test-selector">
+    <ul class="test-lang">
+        <li class="lang-javascript">JavaScript</li>
+        <li class="lang-python">Python</li>
+        <li class="lang-lua">Lua</li>
+    </ul>
+    <ol class="test-lang">
+        <li class="lang-swift">Swift</li>
+        <li class="lang-java">Java</li>
+        <li class="lang-c">C</li>
+    </ol>
 </div>
+分别选择所有语言，所有动态语言，所有静态语言 注： 使用javascript和jquery实现
 
-<script>
-var js = document.getElementById("test-js");
-js.innerText = "JavaScript";
-js.setAttribute("style", "color: #ff0000");
-</script>
+有如下html
+<form id="test-form" action="#0" onsubmit="return false;">
+    <p><label>Name: <input name="name"></label></p>
+    <p><label>Email: <input name="email"></label></p>
+    <p><label>Password: <input name="password" type="password"></label></p>
+    <p>Gender: <label><input name="gender" type="radio" value="m" checked> Male</label> <label><input name="gender" type="radio" value="f"> Female</label></p>
+    <p><label>City: <select name="city">
+    	<option value="BJ" selected>Beijing</option>
+    	<option value="SH">Shanghai</option>
+    	<option value="CD">Chengdu</option>
+    	<option value="XM">Xiamen</option>
+    </select></label></p>
+    <p><button type="submit">Submit</button></p>
+</form>
+输入值后，用jQuery获取表单的JSON字符串，key和value分别对应每个输入的name和相应的value，例如：{"name":"Michael","email":...}
+
+有如下html
+<form id="test-register" action="#" target="_blank" onsubmit="checkRegisterForm()">
+    <p id="test-error" style="color:red"></p>
+    <p>
+        用户名: <input type="text" id="username" name="username">
+    </p>
+    <p>
+        口令: <input type="password" id="password" name="password">
+    </p>
+    <p>
+        重复口令: <input type="password" id="password-2">
+    </p>
+    <p>
+        <button type="submit">提交</button> <button type="reset">重置</button>
+    </p>
+</form>
+利用JavaScript检查用户注册信息是否正确，在以下情况不满足时报错并阻止提交表单：
+
+用户名必须是3-10位英文字母或数字；
+
+口令必须是6-20位；
+
+两次输入口令必须一致。
+
+提示: 编写checkRegisterForm() 函数, 当函数返回值为ture 是表单提交，返回值为false表单不提交 检查失败时调用alert() 弹窗 并返回false
+
+有如下html：
+<!-- HTML结构 -->
+<form id="test-form" action="test">
+    <legend>请选择想要学习的编程语言：</legend>
+    <fieldset>
+        <p><label class="selectAll"><input type="checkbox"> <span class="selectAll">全选</span><span class="deselectAll"><input type="checkbox">全不选</span></label> 
+        <p><label><input type="checkbox" name="lang" value="javascript"> JavaScript</label></p>
+        <p><label><input type="checkbox" name="lang" value="python"> Python</label></p>
+        <p><label><input type="checkbox" name="lang" value="ruby"> Ruby</label></p>
+        <p><label><input type="checkbox" name="lang" value="haskell"> Haskell</label></p>
+        <p><label><input type="checkbox" name="lang" value="scheme"> Scheme</label></p>
+		<p><button type="submit">Submit</button></p>
+    </fieldset>
+</form>
+当用户勾上“全选”时，自动选中所有语言，并把“全选”变成“全不选”；
+
+当用户去掉“全不选”时，自动不选中所有语言；
